1b1605f32c7464799911e7655bea6143a33b1eac
cshao <33412583+shaochennjtu@users.noreply.github.com>
2021-05-14 10:42:43 +0800

    Update go-grep.go

--- go-grep.go
+++ go-grep.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"bufio"
 	"flag"
 	"fmt"
 	"os"
@@ -12,40 +11,20 @@ import (
 	"github.com/shaochennjtu/go-grep/"
 )
 
-var root, query string
-var found = 1
-var wg sync.WaitGroup
+var regexpFlag = flag.Bool("e", false, "Interpret pattern as an regular expression")
 
-func readFile(wg *sync.WaitGroup, path string) {
-	defer wg.Done()
-
-	file, err := os.Open(path)
-	defer file.Close()
-
-	if err != nil {
-		return
+func init() {
+	flag.Usage = func() {
+		fmt.Fprintf(os.Stderr, "%s", internal.UsageText)
+		flag.PrintDefaults()
 	}
-	scanner := bufio.NewScanner(file)
-	for i := 1; scanner.Scan(); i++ {
-		if strings.Contains(scanner.Text(), query) {
-			found = 0
-			fmt.Printf("%s/%s:%d: %s\n", root, path, i, scanner.Text())
-		}
-	}
+	log.SetLevel(log.InfoLevel)
+	flag.Parse()
 }
 
 func main() {
-	flag.Parse()
-	query = flag.Arg(0)
-	root = flag.Arg(1)
-
-	filepath.Walk(root, func(path string, file os.FileInfo, err error) error {
-		if !file.IsDir() {
-			wg.Add(1)
-			go readFile(&wg, path)
-		}
-		return nil
-	})
-	wg.Wait()
-	defer os.Exit(found)
+	internal.Grep(
+		[]byte(flag.Arg(0)),
+		internal.ReadData(),
+		*regexpFlag)
 }
